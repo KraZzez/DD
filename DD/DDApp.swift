d134ae5227503f7d7c4327f940332b011f83b134
Ludvig <krlu21fo@student.ju.se>
2022-12-05 12:58:31 +0100

    Initial Commit

---
+++ DD/DDApp.swift
@@ -0,0 +1,17 @@
+//
+//  DDApp.swift
+//  DD
+//
+//  Created by Ludvig Krantzén on 2022-12-05.
+//
+
+import SwiftUI
+
+@main
+struct DDApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
